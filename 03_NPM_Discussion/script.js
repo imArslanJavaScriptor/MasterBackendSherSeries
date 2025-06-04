447499ffdf695d71b20593e41dcd9873e76c1583
Muhammad Arslan <imarslan7061@gmail.com>
2025-06-04 09:18:18 +0500

    Understanding Node and How it works

---
+++ 03_NPM_Discussion/script.js
@@ -0,0 +1,8 @@
+// NPM Understanding
+// Installing Packages using npm i package Name
+// Uninstalling Packages using npm uninstall package Name
+// installing Packages with specifix version using npm uninstall package Name@version
+// Understaing Scripts and Custom Scripts
+
+
+console.log("Script is Running")
